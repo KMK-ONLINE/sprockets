35c6a700a9d92d4bd7b121d8485a24c245524bd9
Yap Sok Ann <sokann@gmail.com>
2015-03-02 13:56:20 +0700

    Use atomic write to avoid race condition

--- lib/sprockets/cache/file_store.rb
+++ lib/sprockets/cache/file_store.rb
@@ -17,6 +17,8 @@ module Sprockets
       def [](key)
         pathname = @root.join(key)
         pathname.exist? ? pathname.open('rb') { |f| Marshal.load(f) } : nil
+      rescue
+        nil
       end
 
       # Save value to cache
@@ -24,8 +26,58 @@ module Sprockets
         # Ensure directory exists
         FileUtils.mkdir_p @root.join(key).dirname
 
-        @root.join(key).open('w') { |f| Marshal.dump(value, f)}
+        atomic_write(@root.join(key)) do |file|
+          file.write(Marshal.dump(value, file))
+        end
+
         value
+      end
+
+      # extracted from https://github.com/rails/rails/blob/master/activesupport/lib/active_support/core_ext/file/atomic.rb
+      private def atomic_write(file_name, temp_dir = Dir.tmpdir)
+        require 'tempfile' unless defined?(Tempfile)
+        require 'fileutils' unless defined?(FileUtils)
+
+        temp_file = Tempfile.new(File.basename(file_name), temp_dir)
+        temp_file.binmode
+        yield temp_file
+        temp_file.close
+
+        if File.exist?(file_name)
+          # Get original file permissions
+          old_stat = File.stat(file_name)
+        else
+          # If not possible, probe which are the default permissions in the
+          # destination directory.
+          old_stat = probe_stat_in(File.dirname(file_name))
+        end
+
+        # Overwrite original file with temp file
+        FileUtils.mv(temp_file.path, file_name)
+
+        # Set correct permissions on new file
+        begin
+          File.chown(old_stat.uid, old_stat.gid, file_name)
+          # This operation will affect filesystem ACL's
+          File.chmod(old_stat.mode, file_name)
+        rescue Errno::EPERM
+          # Changing file ownership failed, moving on.
+        end
+      end
+
+      private def probe_stat_in(dir)
+        basename = [
+          '.permissions_check',
+          Thread.current.object_id,
+          Process.pid,
+          rand(1000000)
+        ].join('.')
+
+        file_name = File.join(dir, basename)
+        FileUtils.touch(file_name)
+        File.stat(file_name)
+      ensure
+        FileUtils.rm_f(file_name) if file_name
       end
     end
   end
